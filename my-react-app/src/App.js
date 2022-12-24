18e16b1751a50c1e3106a3f903d8ca4321bafb4d
Lucy <lucia.kim0816@gmail.com>
2022-12-23 22:28:08 -0500

    navbar updates

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,8 +1,16 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact />
+        </Routes>
+      </Router>
       <h1>Yo</h1>
     </div>
   );
