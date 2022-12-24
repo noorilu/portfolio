3aafd0e74808d2e272cdcdbdd2450bc021a8ebad
Lucy <lucia.kim0816@gmail.com>
2022-12-24 03:48:19 -0500

    navbar and main page updates

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,17 +1,21 @@
-import "./App.css";
+import styles from "./App.module.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Main from "./pages/main";
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.appContainer}>
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" exact />
+          <Route path="/" element={<Main />} />
         </Routes>
       </Router>
-      <h1>Yo</h1>
+      {/* <h1 class="font-village text-xl">Village Font</h1>
+      <h1 class="font-upheaval text-xl">Upheaval Font</h1>
+      <h1 class="font-birch text-xl">Birch Font</h1>
+      <h1 class="font-blackoak text-xl">Blackoak Font</h1> */}
     </div>
   );
 }
--- my-react-app/src/components/navbar.js
+++ my-react-app/src/components/navbar.js
@@ -1,12 +1,52 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
-          Lucia Kim
+      <div className={styles.navContainer}>
+        <Link to="/">
+          <img
+            src={require("../static/icons/main.PNG")}
+            alt={"apple"}
+            className={styles.navIcon}
+          />
+        </Link>
+        <Link to="/">
+          <img
+            src={require("../static/icons/projects.PNG")}
+            alt={"bunny"}
+            className={styles.navIcon}
+          />
+        </Link>
+        <Link to="/" className={styles.navLogo}>
+          <div className={styles.navTop}>
+            <span>L</span>
+            <span>u</span>
+            <span>c</span>
+            <span>i</span>
+            <span>a</span>
+          </div>
+          <div className={styles.navBottom}>
+            <span>K</span>
+            <span>i</span>
+            <span>m</span>
+          </div>
+        </Link>
+        <Link to="/">
+          <img
+            src={require("../static/icons/art.PNG")}
+            alt={"poo"}
+            className={styles.navIcon}
+          />
+        </Link>
+        <Link to="/">
+          <img
+            src={require("../static/icons/contacts.PNG")}
+            alt={"cactus"}
+            className={styles.navIcon}
+          />
         </Link>
       </div>
     </nav>
